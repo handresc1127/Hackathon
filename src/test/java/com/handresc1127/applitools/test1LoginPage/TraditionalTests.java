976c6495645f67d97d53e96a5b825c36e2aa1fe0
handresc1127 <h.andresc1127@gmail.com>
2019-11-20 12:28:44 -0500

    Change assertions

--- src/test/java/com/handresc1127/applitools/test1LoginPage/TraditionalTests.java
+++ src/test/java/com/handresc1127/applitools/test1LoginPage/TraditionalTests.java
@@ -1,8 +1,5 @@
 package com.handresc1127.applitools.test1LoginPage;
 
-
-import static org.testng.Assert.assertEquals;
-
 import java.util.ArrayList;
 
 import org.openqa.selenium.By;
@@ -11,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -19,9 +16,8 @@ public class TraditionalTests {
 	
 	private WebDriver driver;
 
-	@BeforeMethod
+	@BeforeTest
 	public void beforeEach() {
-		// Use Chrome browser
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
 		driver = new ChromeDriver(options);
@@ -33,29 +29,44 @@ public class TraditionalTests {
 		driver.get(url);
 		SoftAssert sa = new SoftAssert();
 		
-		sa.assertTrue(driver.findElement(By.className("auth-header")) instanceof WebElement);
-		//sa.assertThat(driver.findElement(By.className("auth-header")).getText(), containsString("Login Form"));
+		WebElement user, pass, iconUser, iconPass,labelUser,labelPass,login,alertEmpty,alertPrimary;
+		java.util.List<WebElement> socials;
+		String iconUserClass,iconPassClass;
 		
-		sa.assertTrue(driver.findElement(By.id("username")) instanceof WebElement);
-		sa.assertEquals(driver.findElement(By.id("username")).getAttribute("placeholder"),"Enter your username");
-		sa.assertTrue(driver.findElement(By.xpath("//*[@id='username']/../label")) instanceof WebElement);
-		sa.assertEquals(driver.findElement(By.xpath("//*[@id='username']/../label")).getText(),"Username");
-		//sa.assertTrue(driver.findElement(By.xpath("//*[@id='username']/../*[contains(@class,'pre-icon')]")) instanceof WebElement);
-		//sa.assertThat(driver.findElement(By.xpath("//*[@id='username']/../*[contains(@class,'pre-icon')]")).getAttribute("class"), containsString("os-icon-user-male-circle"));
+		try {user=driver.findElement(By.id("username"));}catch(Exception e){user=null;}
+		try {pass=driver.findElement(By.id("password"));}catch(Exception e){pass=null;}
+		try {iconUser=driver.findElement(By.xpath("//*[@id='username']/../*[contains(@class,'pre-icon')]"));}catch(Exception e){iconUser=null;}
+		try {iconPass=driver.findElement(By.xpath("//*[@id='password']/../*[contains(@class,'pre-icon')]"));}catch(Exception e){iconPass=null;}
+		try {iconUserClass=iconUser.getAttribute("class");}catch(Exception e){iconUserClass=null;}
+		try {iconPassClass=iconPass.getAttribute("class");}catch(Exception e){iconPassClass=null;}
+		try {labelUser=driver.findElement(By.xpath("//*[@id='username']/../label"));}catch(Exception e){labelUser=null;}
+		try {labelPass=driver.findElement(By.xpath("//*[@id='password']/../label"));}catch(Exception e){labelPass=null;}
+		try {login=driver.findElement(By.id("log-in"));}catch(Exception e){login=null;}
+		try {alertEmpty=driver.findElement(By.id("alertEmpty"));}catch(Exception e){alertEmpty=null;}
+		try {alertPrimary=driver.findElement(By.className("alert-primary"));}catch(Exception e){alertPrimary=null;}
+		try {socials=driver.findElements(By.xpath("//form/div[3]/div[2]/*"));}catch(Exception e){socials=null;}
 		
-		sa.assertTrue(driver.findElement(By.id("password")) instanceof WebElement);
-		sa.assertEquals(driver.findElement(By.id("password")).getAttribute("placeholder"),"Enter your password");
-		sa.assertTrue(driver.findElement(By.xpath("//*[@id='password']/../label")) instanceof WebElement);
-		sa.assertEquals(driver.findElement(By.xpath("//*[@id='password']/../label")).getText(),"Password");
-		//sa.assertTrue(driver.findElement(By.xpath("//*[@id='password']/../*[contains(@class,'pre-icon')]")) instanceof WebElement);
-		//sa.assertThat(driver.findElement(By.xpath("//*[@id='password']/../*[contains(@class,'pre-icon')]")).getAttribute("class"), containsString("os-icon-fingerprint"));
 		
-		sa.assertTrue(driver.findElement(By.id("log-in")) instanceof WebElement);
-		sa.assertTrue(driver.findElement(By.id("alertEmpty")) instanceof WebElement);
-		sa.assertTrue(driver.findElement(By.className("alert-primary")) instanceof WebElement);
-		sa.assertTrue(driver.findElements(By.xpath("//form/div[3]/div[2]/*")) instanceof ArrayList);
-		sa.assertEquals(driver.findElements(By.xpath("//form/div[3]/div[2]/*")).size(),3);
-		for (WebElement element: driver.findElements(By.xpath("//form/div[3]/div[2]/*"))) {
+		sa.assertTrue(user instanceof WebElement,"WebElement username is present");
+		sa.assertEquals(user.getAttribute("placeholder"),"Enter your username");
+		sa.assertTrue(labelUser instanceof WebElement,"WebElement label user is present");
+		sa.assertEquals(labelUser.getText(),"Username");
+		sa.assertTrue(iconUser instanceof WebElement,"WebElement icon User is present");
+		sa.assertEquals(iconUserClass, "pre-icon os-icon os-icon-user-male-circle");
+		
+		sa.assertTrue(pass instanceof WebElement,"WebElement password is present");
+		sa.assertEquals(pass.getAttribute("placeholder"),"Enter your password");
+		sa.assertTrue(labelPass instanceof WebElement,"WebElement label pass is present");
+		sa.assertEquals(labelPass.getText(),"Password");
+		sa.assertTrue(iconPass instanceof WebElement,"WebElement icon Pass is present");
+		sa.assertEquals(iconPassClass, "pre-icon os-icon os-icon-fingerprint");
+		
+		sa.assertTrue(login instanceof WebElement,"WebElement login is present");
+		sa.assertTrue(alertEmpty instanceof WebElement,"WebElement alert empty is present");
+		sa.assertTrue(alertPrimary instanceof WebElement,"WebElement alert primary is present");
+		sa.assertTrue(socials instanceof ArrayList,"WebElement socials are present");
+		sa.assertEquals(socials.size(),3);
+		for (WebElement element: socials) {
 			sa.assertEquals(element.getAttribute("href"), url+"#");
 		}
 		// the asserts used till now will not throw any exception if they fail. The @Test will not fail either.
